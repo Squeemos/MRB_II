023fe94db0a5fb575f043b43d8a4872e7ce65162
Squeemos <Squeemos@gmail.com>
2022-09-06 23:34:56 -0700

    Better automation
    Enables us to use an API key instead of OAuth 2.0. Now uses glob to get specific files instead of just os.listdir
    
    Requirements:
    - There cannot be any more txt files inside secret_keys
    - There cannot be any more json files inside secret_keys

--- download_data.py
+++ download_data.py
@@ -12,25 +12,39 @@ import googleapiclient.discovery
 import googleapiclient.errors
 
 import json
+import glob
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-secret_key_dir = "./secret_keys/"
+secret_key_dir = "./secret_keys/*.json"
+api_key_dir = "./secret_keys/*.txt"
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+    # API information
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = secret_key_dir + os.listdir(secret_key_dir)[0]
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
+    # Get the secret file
+    client_secrets_file = glob.glob(secret_key_dir)
+
+    # Extract the api without having to hard code it
+    api_key_file = glob.glob(api_key_dir)
+    with open(api_key_file[0], "r") as api_file:
+        api_key = api_file.read()
+
+    # Get credentials and create an API client (OAuth 2.0)
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    #     client_secrets_file, scopes)
+    # credentials = flow.run_console()
+    # youtube = googleapiclient.discovery.build(
+    #     api_service_name, api_version, credentials=credentials)
+
+    # Create the youtube client with api_key (API_KEY)
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+        api_service_name, api_version, developerKey = api_key)
 
     commands = {
     0 : "Get top 50 videos",
