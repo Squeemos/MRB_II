65e88d5eeebf18874d57569383ea49106411061d
Squeemos <Squeemos@gmail.com>
2022-09-18 19:32:45 -0700

    Beginning start to dashboard
    main_page.py : Simple beginning to a streamlit dashboard. Currently just displays a selected video with over 10 million views' thumbnail
    
    Requirements.txt : Updated to include all requirements
    
    download_data.py : Removed the function for local pathing, as it's not needed anymore

--- download_data.py
+++ download_data.py
@@ -15,22 +15,13 @@ import googleapiclient.errors
 import json
 import glob
 
-def get_local_path(local_path : str) -> str:
-    '''
-        Given a string that looks like:
-            - "./local/path/to/folder/"
-            - "./local/path/to/file.extension"
-        return a string to the path that's OS independent
-    '''
-    return os.path.join(str(pathlib.Path(__file__).parent.resolve()), local_path)
-
 def main():
     scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
-    secret_key_dir = get_local_path("./secret_keys/*.json")
-    api_key_dir = get_local_path("./secret_keys/*.txt")
-    data_path = get_local_path("./data/")
-    resources_path = get_local_path("./resources/")
+    secret_key_dir = "./secret_keys/*.json"
+    api_key_dir = "./secret_keys/*.txt"
+    data_path = "./data/"
+    resources_path = "./resources/"
 
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
