d7976a3b87635fe4fbd1e335c712bc83ea85fd46
Squeemos <56849503+Squeemos@users.noreply.github.com>
2022-09-20 16:13:17 -0700

    Scrub 30 days ago data
    Added in a small thing to scrub data that is 30 days old or more

--- experiments/scripts/automate_download_data.py
+++ experiments/scripts/automate_download_data.py
@@ -5,13 +5,14 @@
 # https://developers.google.com/explorer-help/code-samples#python
 
 import os
-from datetime import datetime
-import pathlib
+from datetime import datetime, timedelta
+from pytz import timezone, UTC
+
+import tinydb
+from reader import YouTubeReader
 
 import googleapiclient.discovery
 import googleapiclient.errors
-
-import json
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -19,8 +20,8 @@ def main():
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-    api_key_path = "../api_key.txt"
-    output_path = "../data/"
+    api_key_path = "../../secret_keys/api_key.txt"
+    output_path = "./database/"
 
     # API information
     api_service_name = "youtube"
@@ -30,27 +31,35 @@ def main():
     with open(api_key_path, "r") as api_file:
         api_key = api_file.read()
 
-    # Create the youtube client with api_key (API_KEY)
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = api_key)
+    # # Create the youtube client with api_key (API_KEY)
+    # youtube = googleapiclient.discovery.build(
+    #     api_service_name, api_version, developerKey = api_key)
+    #
+    # request = youtube.videos().list(
+    #     part="id,snippet,contentDetails,status,statistics,player,topicDetails,recordingDetails,liveStreamingDetails,localizations",
+    #     chart="mostPopular",
+    #     maxResults=50,
+    #     regionCode="US"
+    # )
+    # response = request.execute()
 
-    request = youtube.videos().list(
-        part="id,snippet,contentDetails,status,statistics,player,topicDetails,recordingDetails,liveStreamingDetails,localizations",
-        chart="mostPopular",
-        maxResults=50,
-        regionCode="US",
-        prettyPrint=True
-    )
-    response = request.execute()
-    time = datetime.now()
-    time_str = time.strftime("%m-%d-%Y_%H-%M-%S")
-    for item in response["items"]:
-        item["queryTime"] = time_str
-        item["queryTimestamp"] = time.timestamp()
+    with tinydb.TinyDB(output_path + "youtube.json", indent = 2) as database:
+        YtR = YouTubeReader()
+        trending = database.table("TRENDING")
+        # YtR.insert_videos(response, trending)
 
+        thirty_days_ago = datetime.now(timezone("UTC")) - timedelta(days = 2)
+        query = tinydb.Query()
+        convert_to_dt = lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo = UTC)
+        result = trending.search(query.queryTime.map(convert_to_dt) < thirty_days_ago)
+        doc_ids = [r.doc_id for r in result]
+        trending.remove(doc_ids = doc_ids)
 
-    with open(output_path + f"{time_str}_output.json", "w") as outfile:
-        json.dump(response, outfile, indent = 4)
+        new_result = trending.search(query.queryTime.map(convert_to_dt) < thirty_days_ago)
+        print(len(new_result))
+
+    # with open(output_path + f"{time_str}_output.json", "w") as outfile:
+    #     json.dump(response, outfile, indent = 4)
 
 if __name__ == "__main__":
     main()
---
+++ experiments/scripts/reader.py
@@ -0,0 +1,134 @@
+"""Contains YouTubeReader class that inserts YTD API data in TinyDB formats.
+"""
+
+from typing import Union
+
+import json
+
+from datetime import datetime
+from pytz import timezone
+
+from tinydb import TinyDB
+from tinydb.table import Table
+
+
+class YouTubeReader:
+    """Reader for inserting YouTube Data API responses into TinyDB formats.
+    """
+
+    # Features to Encode -------------------------------------------------------
+
+    int_names = (
+        "categoryId",
+        "viewCount",
+        "likeCount",
+        "favoriteCount",
+        "commentCount",
+    )
+
+    bool_names = (
+        "caption",
+        "licensedContent",
+        "embeddable",
+        "publicStatsViewable",
+        "madeForKids",
+    )
+
+    dt_names = (
+        "publishedAt",
+    )
+
+    # Video Data ---------------------------------------------------------------
+
+    def insert_videos(self, data: Union[str, dict], table: Union[TinyDB, Table]):
+        """Inserts video data into the given ytdb or table.
+
+        Each video in the given API query result is inserted separately into
+        the given table as a new sample.
+
+        Each inserted sample is given a UTC timestamp. Additionally, each of
+        the query 'parts' (i.e. 'statistics', 'snippet') are flattened. Some
+        fields' data type are converted for easier TinyDB querying.
+
+        Parameters:
+            data: YT API response JSON either as filepath or loaded dict
+            table: TinyDB ytdb or table which supports insertion
+        """
+        # If filepath given, convert .json at location to dict
+        if isinstance(data, str):
+            with open(data, "r") as file:
+                data = json.load(file)
+
+        # Create time string from current UTC time and f
+        dt = datetime.now(timezone("UTC"))
+        time = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+        timestamp = dt.timestamp()
+
+        # Create individual entries
+        entries = []
+        for item in data["items"]:
+            item = self._flatten_dict(item)
+
+            # Add time data to video
+            entry = {"queryTime": time, "queryTimestamp": timestamp}
+            entry.update(item)
+
+            # Convert fields prior to insertion
+            self._encode_fields(entry)
+
+            entries.append(entry)
+
+        # Insert new entries into the table
+        table.insert_multiple(entries)
+
+    @staticmethod
+    def _encode_fields(entry: dict):
+        """Encodes some integer, boolean, datetime fields
+
+        Parameters
+            entry: Individual video entry to be inserted in table.
+        """
+        # Integer fields
+        for int_name in YouTubeReader.int_names:
+            try:
+                entry[int_name] = int(entry[int_name])
+            except (KeyError, ValueError):
+                pass
+
+        # Boolean fields
+        for bool_name in YouTubeReader.bool_names:
+            try:
+                if isinstance(entry[bool_name], str):
+                    entry[bool_name] = bool(entry[bool_name])
+            except (KeyError, ValueError):
+                pass
+
+        # Datetime fields
+        for dt_name in YouTubeReader.dt_names:
+            try:
+                ts = datetime.strptime(entry[dt_name], "%Y-%m-%dT%H:%M:%SZ").timestamp()
+                entry[f"{dt_name}Timestamp"] = ts
+            except (KeyError, TypeError):
+                pass
+
+        return entry
+
+    # Helpers ------------------------------------------------------------------
+
+    @staticmethod
+    def _flatten_dict(data: dict):
+        """Flattens first level of dictionary."""
+        flat = dict()
+
+        # For items in orignal json
+        for k1, v1 in data.items():
+            if isinstance(v1, dict):
+                # For items in part (ie snippet)
+                for k2, v2 in v1.items():
+                    # ex. title, description
+                    flat[k2] = v2
+            else:
+                # ex. kind, etag, id
+                flat[k1] = v1
+
+        return flat
