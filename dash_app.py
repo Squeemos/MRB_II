08fb247ca23d0eef2f35153ddd7f3b468268d5b9
Squeemos <Squeemos@gmail.com>
2022-10-03 21:00:25 -0700

    Proper memory storage

--- dash_app.py
+++ dash_app.py
@@ -9,32 +9,53 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
+import tinydb
+from ytdb.storage import OnlineBetterJSONStorage
+
 app = Dash()
-global_df = pd.read_pickle("https://squeemos.pythonanywhere.com/static/local_storage.xz")
 
 app.layout = html.Div(children=[
     html.H1(children = "YouTube API Dashboard"),
 
     html.Div(children = "A simple dashboard for ineracting with and exploring YouTube API data"),
 
+    html.Div([
+        dcc.Dropdown(id = "data-set", multi = False, value = "TRENDING",
+            options = [{"label" : "TRENDING" , "value" : "TRENDING"}]
+        )
+    ]),
+
     dcc.Slider(1, 20, 1,
                value = 5,
                id = "view_slider"
     ),
 
-    dcc.Graph(
-        id = "views_based_on_slider"
-    )
+    dcc.Store(id = "dataframe", storage_type = "memory", data = []),
+    dcc.Graph(id = "views_based_on_slider")
 ])
 
 @app.callback(
+    Output("dataframe", "data"),
+    Input("data-set", "value")
+)
+def generate_dataframe(value):
+    data_base = tinydb.TinyDB("https://squeemos.pythonanywhere.com/static/youtube.json", storage = OnlineBetterJSONStorage)
+    df = pd.DataFrame(data_base.table(value))
+    return df.to_dict()
+    #df = pd.read_pickle("https://squeemos.pythonanywhere.com/static/local_storage.xz")
+    #return df.to_dict(), {}
+
+@app.callback(
     Output("views_based_on_slider", "figure"),
-    Input("view_slider", "value"))
-def update_view_count_graph(view_slider):
+    [Input("view_slider", "value"),
+    Input("dataframe", "data")])
+def update_view_count_graph(view_slider, data):
     video_views = int(view_slider) * 1_000_000
 
-    ids = global_df[global_df["viewCount"] >= video_views]["id"].unique()
-    result = global_df[global_df["id"].isin(ids)]
+    df = pd.DataFrame(data)
+
+    ids = df[df["viewCount"] >= video_views]["id"].unique()
+    result = df[df["id"].isin(ids)]
     new_fig = px.line(result,
         x = "queryTime",
         y = "viewCount",
