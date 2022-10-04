2523cbefc446be87c6465547c2e7a734cdbf8a51
Squeemos <Squeemos@gmail.com>
2022-10-03 21:19:01 -0700

    Real dashapp with caching
    It's fixed

--- dash_app.py
+++ dash_app.py
@@ -9,53 +9,50 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
-import tinydb
-from ytdb.storage import OnlineBetterJSONStorage
-
 app = Dash()
 
-app.layout = html.Div(children=[
+app.layout = html.Div(children = [
     html.H1(children = "YouTube API Dashboard"),
-
     html.Div(children = "A simple dashboard for ineracting with and exploring YouTube API data"),
 
-    html.Div([
-        dcc.Dropdown(id = "data-set", multi = False, value = "TRENDING",
-            options = [{"label" : "TRENDING" , "value" : "TRENDING"}]
-        )
-    ]),
-
+    # Simple slider to select values
+    # To get the value from the slider, look at "id" and read "value"
     dcc.Slider(1, 20, 1,
                value = 5,
                id = "view_slider"
     ),
 
+    # Storage of the dataframe in memory, valid until refreshed or closed
     dcc.Store(id = "dataframe", storage_type = "memory", data = []),
+    # The graph, to display the graph here, output "figure" to "id"
     dcc.Graph(id = "views_based_on_slider")
 ])
 
+# Simple callback that stores the dataframe
 @app.callback(
     Output("dataframe", "data"),
-    Input("data-set", "value")
+    Input("dataframe", "data")
 )
 def generate_dataframe(value):
-    data_base = tinydb.TinyDB("https://squeemos.pythonanywhere.com/static/youtube.json", storage = OnlineBetterJSONStorage)
-    df = pd.DataFrame(data_base.table(value))
-    return df.to_dict()
-    #df = pd.read_pickle("https://squeemos.pythonanywhere.com/static/local_storage.xz")
-    #return df.to_dict(), {}
+    return pd.read_pickle("https://squeemos.pythonanywhere.com/static/yt_trending.xz").to_dict()
 
+# Better callback to dispolay some things
+# Output is a figure to the dcc.Graph
+# Input is the slider and the dataframe
 @app.callback(
     Output("views_based_on_slider", "figure"),
     [Input("view_slider", "value"),
     Input("dataframe", "data")])
 def update_view_count_graph(view_slider, data):
+    # Convert to int and the dataframe
     video_views = int(view_slider) * 1_000_000
-
     df = pd.DataFrame(data)
 
+    # Perform the query
     ids = df[df["viewCount"] >= video_views]["id"].unique()
     result = df[df["id"].isin(ids)]
+
+    # Create a figure with plotly express
     new_fig = px.line(result,
         x = "queryTime",
         y = "viewCount",
