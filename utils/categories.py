5369f8aa2af31adb65543991022ab6037c777c1a
Squeemos <56849503+Squeemos@users.noreply.github.com>
2022-10-09 14:56:58 -0700

    Better categories object with operator overloads

--- utils/categories.py
+++ utils/categories.py
@@ -1,9 +1,21 @@
-# Categories
-from enum import Enum
 import json
 from urllib.request import urlopen
 
 class YouTubeCategories(object):
+    class YouTubeCategory(object):
+        def __init__(self, key, value):
+            self.__key = key
+            self.__value = value
+
+        def __eq__(self, other):
+            return other in self.__value
+
+        def __str__(self):
+            return f"{self.__key} : {self.__value}"
+
+        def __repr__(self):
+            return f"YouTubeCategory: <Key, {self.__key}>, <Value, {self.__value}>"
+
     def __init__(self, path, local = False):
         if local:
             with open(path) as link:
@@ -11,19 +23,27 @@ class YouTubeCategories(object):
         else:
             with urlopen(path) as link:
                 data = json.load(link)
-        self.__dictionary = {item["snippet"]["title"] : int(item["id"]) for item in data["items"]}
-        self.__categories = Enum("Categories", self.__dictionary)
+
+        self.__id_to_title = {int(item["id"]) : item["snippet"]["title"] for item in data["items"]}
+
+        self.__title_to_id = {}
+        for key, value in self.__id_to_title.items():
+            if value in self.__title_to_id:
+                self.__title_to_id[value].append(key)
+            else:
+                self.__title_to_id[value] = [key]
 
     def __getitem__(self, item):
-        return self.__categories[item]
-
-    def get_list(self):
-        return list(self.__dictionary.keys())
+        return YouTubeCategories.YouTubeCategory(item, self.__title_to_id[item])
 
     @property
-    def t_to_i(self):
-        return self.__dictionary
+    def titles(self) -> list:
+        return list(self.__title_to_id.keys())
 
     @property
-    def i_to_t(self):
-        return {v:k for k,v in self.__dictionary.items()}
+    def title_to_id(self) -> dict:
+        return self.__title_to_id
+
+    @property
+    def id_to_title(self) -> dict:
+        return self.__title_to_id
