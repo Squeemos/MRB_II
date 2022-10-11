7a9cfbdc627ec57dd0a9a6600836c8e364aade5a
Squeemos <Squeemos@gmail.com>
2022-10-10 22:56:26 -0700

    I think it works now

--- dash_app.py
+++ dash_app.py
@@ -1,8 +1,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-# Dumb comment to test the action with
-
 # Dash things
 from dash import Dash, html, dcc, Input, Output, State, dash_table
 from flask_caching import Cache
