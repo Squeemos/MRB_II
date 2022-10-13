a1d0291402049862f4073e90ca5e291ff7769894
Squeemos <56849503+Squeemos@users.noreply.github.com>
2022-10-13 03:27:08 -0700

    Updates to the dashboard to account for feather
    Since everything uses feather now, make a change to use feather instead. Also, add in something to do that uses the new categories data.

--- dash_app.py
+++ dash_app.py
@@ -22,7 +22,7 @@ cache = Cache()
 
 cache.init_app(app.server, config = total_config["CACHE_CONFIG"])
 
-categories = YouTubeCategories(total_config["PATHS"]["CATEGORIES"], local = total_config["LOCAL"])
+categories = YouTubeCategories(total_config["PATHS"]["CATEGORY_IDS"], local = total_config["LOCAL"])
 
 app.layout = html.Div(children = [
     html.H1(children = "YouTube API Dashboard"),
@@ -44,13 +44,19 @@ app.layout = html.Div(children = [
 
     # The graph, to display the graph here, output "figure" to "id"
     dcc.Graph(id = "views_based_on_slider"),
-    dcc.Graph(id = "bar_chart_categories")
+    dcc.Graph(id = "bar_chart_categories"),
+
+    dcc.Dropdown(
+        options = categories.titles,
+        id = "trending_category_id",
+    ),
+    dcc.Graph(id = "category_trending"),
 ])
 
 # Get and memoize the dataframe
 @cache.memoize(timeout = 3000)
 def get_dataframe(url):
-    return pd.read_pickle(url)
+    return pd.read_feather(url)
 
 # Better callback to display some things
 # Output is a figure to the dcc.Graph
@@ -62,7 +68,7 @@ def get_dataframe(url):
 def update_view_count_graph(view_slider, category_id):
     # Convert to int and the dataframe
     video_views = int(view_slider) * 1_000_000
-    df = get_dataframe(total_config["PATHS"]["DF"])
+    df = get_dataframe(total_config["PATHS"]["TRENDING"])
 
     # Perform the query
     ids = df[df.yt["viewCount"] >= video_views]["id"].unique()
@@ -92,15 +98,17 @@ def update_view_count_graph(view_slider, category_id):
     [Input("category_id", "value")]
 )
 def update_bar_chart_categories(category_id):
-    df = get_dataframe(total_config["PATHS"]["DF"])
+    df = get_dataframe(total_config["PATHS"]["TRENDING"])
     df = df.drop_duplicates(df.yt.get_alias("id"))
-    grouped = df.groupby(df.yt.get_alias("categoryId")).size().reset_index()
+    grouped = df.groupby(df.yt.get_alias("categoryId")).size().reset_index().copy()
     grouped.columns = [grouped.yt.get_alias("categoryId"), "count"]
     if category_id is not None and len(category_id) != 0:
         category_ids = categories[category_id]
-        grouped = grouped[grouped.yt["categoryId"].isin(category_ids)]
 
-    grouped[grouped.yt.get_alias("categoryId")] = grouped[grouped.yt.get_alias("categoryId")].apply(lambda x: categories.id_to_title[x])
+        # Prevent copy problem
+        grouped = grouped[grouped.yt["categoryId"].isin(category_ids)].copy()
+
+    grouped[grouped.yt.get_alias("categoryId")] = grouped[grouped.yt.get_alias("categoryId")].map(categories.id_to_title)
 
     new_fig = px.bar(
         grouped,
@@ -114,6 +122,33 @@ def update_bar_chart_categories(category_id):
     )
 
     return new_fig
+
+@app.callback(
+    Output("category_trending", "figure"),
+    [Input("trending_category_id", "value")]
+)
+def update_category_trending(category_trending):
+    if category_trending is not None and category_trending != [None]:
+        df = get_dataframe(total_config["PATHS"]["CATEGORIES"])
+        df = df[df.yt["categoryId"] == categories[category_trending]]
+
+        new_fig = px.line(
+            df,
+            x = df.yt.get_alias("queryTime"),
+            y = df.yt.get_alias("viewCount"),
+            color = df.yt.get_alias("title"),
+            title = f"Videos from {category_trending} trending tab",
+            labels = {
+                df.yt.get_alias("queryTime") : "Date",
+                df.yt.get_alias("viewCount") : "Views",
+                df.yt.get_alias("title") : "Title",
+            }
+        )
+
+        return new_fig
+    else:
+        return {}
+
 
 
 if __name__ == '__main__':
