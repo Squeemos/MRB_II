178ac35ddd603c2ca14f275340de1f610afc1b22
Squeemos <56849503+Squeemos@users.noreply.github.com>
2022-10-17 00:34:42 -0700

    Multiprocessing, beginning of thumbnail classification

--- scripts/download_videos_mp.py
+++ scripts/download_videos_mp.py
@@ -5,13 +5,23 @@ import os
 from PIL import Image
 from requests import get
 import time
+from itertools import repeat
 
 from multiprocessing import Pool
 import argparse
 
 from yt_utils import YouTubeAccessor
 
+# Download a video from a url
 def download_video(url, what, local_path, key, counter):
+    '''
+        Arguments:
+            url: str url to the image
+            what: the thing this specific image belongs to (ex. category)
+            local_path: the local path to save the image to (must exist)
+            key: used to process the image a specific way
+            counter: a counter for a unique number to name the file
+    '''
     img = Image.open(get(url, stream = True).raw)
     arr = np.array(img)
     if "high" in key:
@@ -29,22 +39,30 @@ def main():
 
     args = parser.parse_args()
 
+    print("Downloading dataframe...")
     if args.url:
         df = pd.read_feather(args.url)
     else:
         raise Exception("Must pass in url")
+    print("Dataframe downloaded\nBeginning extraction...")
 
+    # Process the data and set it up to become arguments for the function
     df = df.drop_duplicates(subset = df.yt.get_alias(args.key))
+    df = df[df.yt[args.key] != None]
     df = df[[df.yt.get_alias(args.key), df.yt.get_alias(args.what)]]
     range_len = range(len(df))
 
+    print("Extraction complete")
+
+    # Create an iterable with all the function arguments
     arrs = zip(df.yt[args.key].values,
                df.yt[args.what],
-               [args.local_path for _ in range_len],
-               [args.key for _ in range_len],
+               repeat(args.local_path),
+               repeat(args.key),
                range(len(df))
            )
 
+    # Create the folders if they don't exist
     if not os.path.exists(args.local_path):
         os.mkdir(args.local_path)
 
@@ -53,12 +71,13 @@ def main():
             os.mkdir(f"{args.local_path}/{val}")
 
     start_time = time.perf_counter()
+    # Create pool
     with Pool(args.num_processes) as p:
         print("Starting pool...")
-        if "high" in args.key:
-            p.starmap(download_video, arrs)
+        # Asynchronous processing
+        p.starmap_async(download_video, arrs).get()
         print("Processes completed")
-    print(f"Took {time.perf_counter() - start_time:.4} seconds")
+    print(f"Took {time.perf_counter() - start_time:.2} seconds")
 
 if __name__ == "__main__":
     main()
