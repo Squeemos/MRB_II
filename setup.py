e451ff0bc680ed28f7b43337165bc8dbdad08729
Squeemos <Squeemos@gmail.com>
2022-10-30 16:50:01 -0700

    Update to setup
    Blacklisted packages

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 
+blacklisted = ["models", "etl"]
+
 p = find_packages()
 p = [x for x in p if not "online" in x]
+p = [x for x in p if x not in blacklisted]
 
 setup(name = 'MRB', version = '1.0', packages = p)
