8350a4d9af0c94e0651ceb26f13790268829acb8
Eric Zander <60997515+ericzander@users.noreply.github.com>
2022-12-22 16:18:05 -0800

    Created MySQL reader and download script
    Created a new YoutubeReader for MySQL with an accompanying download script

---
+++ mysql/automate_download_data.py
@@ -0,0 +1,163 @@
+"""Script for downloading data with the YouTube Data API client."""
+
+import json
+
+from reader import YouTubeReader
+from datetime import datetime
+from pytz import timezone
+
+import googleapiclient.discovery
+import googleapiclient.errors
+
+def main():
+    try:
+        downloader = YoutubeDownloader("eric", "localhost", "mysql_key.txt", "./data/")
+
+        # Update category ids
+        category_ids = downloader.get_category_ids()
+
+        # Update trending videos
+        downloader.get_trending()
+
+        # Update category videos
+        downloader.get_categories(category_ids)
+
+        print("done")
+
+    # Something went wrong, log the exception
+    except Exception as e:
+        with open("./error.log", "a") as f:
+            dt = datetime.now(timezone("UTC")).strftime("%Y-%m-%dT%H:%M:%SZ")
+            print(f"{dt}: {type(e)}: {e}")
+            f.write(f"{dt}: {type(e)}: {e}\n")
+
+class YoutubeDownloader:
+    def __init__(self, username: str, hostname: str, key_filename: str, output_path: str) -> None:
+        # Build client
+        self.youtube_client = self._build_client()
+
+        # Prepare database info
+        with open(key_filename, "r") as file:
+            self.key = file.read()
+        self.username = username
+        self.hostname = hostname
+
+        # Prepare file IO
+        self.output_path = output_path
+
+    def get_trending(self, parts: str = None):
+        """Gets top trending videos in the US."""
+        print("getting trending videos...")
+
+        youtube_reader = YouTubeReader(self.username, self.key, self.hostname, "trending")
+
+        if parts is None:
+            parts = self._get_all_parts()
+
+        # Get data
+        request = self.youtube_client.videos().list(
+            part=parts,
+            chart="mostPopular",
+            maxResults=50,
+            regionCode="US"
+        )
+        response = request.execute()
+        print("got trending videos")
+
+        # Insert the new videos into table
+        youtube_reader.insert_videos(response)
+        print("trending videos inserted")
+
+    def get_category_ids(self):
+        """Saves raw response data for video categories and returns list of ids."""
+        print("getting category ids...")
+
+        request = self.youtube_client.videoCategories().list(
+            part="snippet",
+            regionCode="US"
+        )
+        response = request.execute()
+        print("got category ids")
+
+        with open(self.output_path + "video_categories.json", "w") as outfile:
+            json.dump(response, outfile, indent=2)
+        print("saved category ids")
+
+        # Return categories
+        category_ids = [cat["id"] for cat in response["items"]]
+
+        return category_ids
+
+    def get_categories(self, category_ids, parts: str = None):
+        """Gets top videos for all categories in the US that support this."""
+        print("getting category videos...")
+
+        youtube_reader = YouTubeReader(self.username, self.key, self.hostname, "categories")
+
+        if parts is None:
+            parts = self._get_all_parts()
+
+        for cat_id in category_ids:
+            try:
+                # Get data
+                request = self.youtube_client.videos().list(
+                    part=parts,
+                    chart="mostPopular",
+                    maxResults=50,
+                    regionCode="US",
+                    videoCategoryId=cat_id,
+                )
+                response = request.execute()
+                print(f"got cat{cat_id} videos")
+
+                # Insert the new videos into table
+                youtube_reader.insert_videos(response)
+                print(f"cat{cat_id} videos inserted")
+            except googleapiclient.errors.HttpError:
+                print(f"cat{cat_id} chart not found or failed")
+
+        print("categories saved")
+
+    @staticmethod
+    def _build_client():
+        """Creates and returns the YouTube Data API client needed for requests."""
+        print("building client...")
+
+        api_key_path = "api_key.txt"
+
+        # API information
+        api_service_name = "youtube"
+        api_version = "v3"
+
+        # Read API key
+        with open(api_key_path, "r") as api_file:
+            api_key = api_file.read()
+
+        # Create the client with api_key
+        youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey=api_key
+        )
+        print("client built")
+
+        return youtube
+
+    @staticmethod
+    def _get_all_parts():
+        parts = (
+            "id",
+            "snippet",
+            "contentDetails",
+            "status",
+            "statistics",
+            "player",
+            "topicDetails",
+            "recordingDetails",
+            "liveStreamingDetails",
+            "localizations",
+        )
+
+        return ",".join(parts)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mysql/reader.py
@@ -0,0 +1,180 @@
+"""Contains YouTubeReader that inserts YTD API data into MySQL Database."""
+
+from datetime import datetime
+from pytz import timezone
+
+import pandas as pd
+
+import sqlalchemy as sqa
+
+
+class YouTubeReader:
+    """Reader for inserting YouTube Data API responses into database.
+    """
+    # Table Schemas --------------------------------------------------------------------------
+
+    video_schema = {
+        "queryNum": sqa.Integer,
+        "queryTime": sqa.DateTime,
+
+        "id": sqa.Text,
+        
+        "snippet.categoryId": sqa.Integer,
+        "snippet.publishedAt": sqa.DateTime,
+        "snippet.channelId": sqa.Text,
+        "snippet.title": sqa.Text,
+        "snippet.description": sqa.Text,
+        "snippet.channelTitle": sqa.Text,
+        
+        "statistics.viewCount": sqa.BigInteger,
+        "statistics.likeCount": sqa.BigInteger,
+        "statistics.commentCount": sqa.BigInteger,
+
+        "contentDetails.duration": sqa.BigInteger, 
+    }
+
+    tag_schema = {
+        "queryNum": sqa.Integer,
+
+        "id": sqa.Text,
+        
+        "snippet.tags": sqa.Text, 
+    }
+
+    thumbnail_schema = {
+        "queryNum": sqa.Integer,
+
+        "id": sqa.Text,
+        
+        "default.url": sqa.Text,
+        "default.width": sqa.Integer,
+        "default.height": sqa.Integer,
+
+        "medium.url": sqa.Text,
+        "medium.width": sqa.Integer,
+        "medium.height": sqa.Integer,
+
+        "high.url": sqa.Text,
+        "high.width": sqa.Integer,
+        "high.height": sqa.Integer,
+
+        "standard.url": sqa.Text,
+        "standard.width": sqa.Integer,
+        "standard.height": sqa.Integer,
+
+        "maxres.url": sqa.Text,
+        "maxres.width": sqa.Integer,
+        "maxres.height": sqa.Integer,
+    }
+
+    # Constructor/Destructor----------------------------------------------------
+
+    def __init__(self, username: str, password: str, hostname: str, dbname: str,
+                 query_num_table_name: str = "videos"):
+        # Create engine
+        #   engine = create_engine("mysql://user:pwd@host/db_name", echo=True)
+        connect_str = f"mysql://{username}:{password}@{hostname}/{dbname}"
+        self.engine = sqa.create_engine(connect_str, echo=False)
+
+        # Save query information
+        self.query_number = self._get_query_number(self.engine, query_num_table_name)
+        self.query_time = datetime.now(timezone("UTC")).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # Video Data ---------------------------------------------------------------
+
+    def insert_videos(self, data: dict):
+        # Read client response into flattened dataframe
+        df = pd.json_normalize(data["items"], max_level=1)
+
+        # Save query specific features
+        df["queryNum"] = self.query_number
+        df["queryTime"] = self.query_time
+
+        # Perform insertions
+        self._insert_vids(df)
+        self._insert_tags(df)
+        self._insert_thumbnails(df)
+
+    def _insert_vids(self, df):
+        #self.engine.execute("DROP TABLE IF EXISTS videos")
+        self._create_table("videos", self.video_schema)
+
+        # Get only features that matter
+        df_vid = df.loc[:, list(self.video_schema.keys())]
+
+        # Convert duration to raw seconds
+        df_vid["contentDetails.duration"] = pd.to_timedelta(df_vid["contentDetails.duration"].str.slice(start=2).str.replace("M", "m")).dt.seconds
+
+        # Convert columns to datetime
+        dt_names = (
+            "queryTime",
+            "snippet.publishedAt",
+        )
+        for dt_feat in dt_names:
+            df_vid[dt_feat] = pd.to_datetime(df_vid[dt_feat], utc=True)
+
+        # Insert into database
+        df_vid.to_sql("videos", self.engine, index=False, if_exists="append",
+                      dtype=self.video_schema)
+
+        return df
+
+    def _insert_tags(self, df):
+        self._create_table("tags", self.tag_schema)
+
+        # Create tags dataframe
+        df_tags = df.loc[:, ["id", "snippet.tags"]]
+        df_tags = df_tags.explode("snippet.tags")
+        df_tags["queryNum"] = self.query_number
+
+        # Insert into database
+        df_tags.to_sql("tags", self.engine, index=False, if_exists="append",
+                       dtype=self.tag_schema)
+
+    def _insert_thumbnails(self, df):
+        self._create_table("thumbnails", self.thumbnail_schema)
+
+        # Get thumbnail data
+        df_thumb = pd.json_normalize(df["snippet.thumbnails"])
+        df_thumb["queryNum"] = self.query_number
+
+        # Get id from thumbnail name
+        df_thumb["id"] = df_thumb["default.url"].str.split("/").apply(lambda x: x[-2])
+
+        df_thumb.to_sql("thumbnails", self.engine, index=False, if_exists="append",
+                        dtype=self.thumbnail_schema)
+
+
+    # Helpers ------------------------------------------------------------------
+
+    @staticmethod
+    def _get_query_number(engine: sqa.engine.base.Engine, table_name: str,
+                          query_feat_name: str = "queryNum"):
+        try:
+            query_number = pd.read_sql(f"SELECT MAX({query_feat_name}) FROM {table_name};", engine)
+            query_number = query_number.iloc[0][0]
+            query_number = query_number + 1 if query_number is not None else 1
+        except:
+            query_number = 1
+
+        return query_number
+
+    def _create_table(self, name: str, features: dict):
+        """Given a name and a dictionary of features, creates and returns a table with SQLAlchemy.
+
+        Params:
+            name: Name of resulting table
+            features: Dictionary of feature names as keys and SQLAlchemy data types as values
+        """
+        meta = sqa.MetaData()
+
+        table = sqa.Table(
+            name, meta,
+            sqa.Column("num", sqa.Integer, primary_key=True),
+            sqa.Column("queryNum", sqa.Integer, index=True),
+            *[sqa.Column(name, dtype) for name, dtype in features.items()],
+        )
+
+        meta.create_all(self.engine)
+
+        return table
